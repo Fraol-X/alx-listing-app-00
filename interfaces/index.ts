85eae236b842305d84399bc388c21119d62e0e9a
Fraol-X <pfresh641@gmail.com>
2024-12-21 09:59:43 +0300

    Responsive Listing Page

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -11,3 +11,29 @@ export interface ButtonProps {
     label: string;
     onClick: ()=> void;
 }
+
+export interface address {
+    state: string;
+    city: string;
+    country: string;
+}
+export interface offers {
+    bed: string;
+    shower: string;
+    occupants: string;
+}
+export interface PropertyProps {
+    name: string;
+    address: address;
+    rating: string;
+    category: string[];
+    price: number;
+    offers: offers;
+    image: string;
+    discount: string;
+}
+export interface PillProps {
+    label: string;
+    isSelected: boolean;
+    onClick: () => void;
+}
